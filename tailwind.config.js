139043125cc0c07068215736a8c6bdfb85559187
aihki <laine.nestori.julius@gmail.com>
2025-09-30 14:27:22 +0300

    refactor: update color palette in Tailwind configuration for improved theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#006B67',
+        main_light_turquoise: '#64C3CD',
+        main_medium_turquise: '#00AAA3',
+        main_dark_turquoise: '#008782',
+        main_black: '#2C2A29',
+        support_pink: '#D8318A',
+        support_red: '#BD3429',
+        support_dark_orange: '#BE4D00',
+        support_orange: '#EA7600',
+        support_yellow: '#F9D800',
+        support_brown: '#4E3629',
+        support_dark_green: '#006431',
+        support_medium_green: '#009F4D',
+        support_light_green: '#74AA50',
+        support_purple: '#814494',
+        support_dark_blue: '#006AA7',
+        support_light_blue: '#9BCBEB',
+
+
       },
       fontFamily: {
         sans: ['Inter', 'Arial', 'sans-serif'],
