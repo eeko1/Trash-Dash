2d268425c8ddf4103624c5961cc7de17175e62a9
aihki <laine.nestori.julius@gmail.com>
2025-11-11 12:15:33 +0200

    feat: added point context that can be use to send the points in play section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,22 +7,26 @@ import DropGame from './views/Dropgame';
 import TrashOrSmash from './views/Trashorsmash'
 import Recydle from './views/Recydle';
 import { UserProvider } from './contexts/UserContext';
+import { PointSender } from './contexts/PointContext';
+
 
 
 function App() {
    return (
       <UserProvider>
-         <Router>
-            <Routes>
-               <Route path='/' element={<Home />} />
-               <Route path='/PickTheGame' element={<PickTheGame />} />
-               <Route path='/RecyclingGuide' element={<RecycleGuide />} />
-               <Route path='/Leaderboard' element={<Leaderboard />} />
-               <Route path='/DropGame' element={<DropGame />} />
-               <Route path='/TrashOrSmash' element={<TrashOrSmash />} />
-               <Route path='/Recydle' element={<Recydle />} />
-            </Routes>
-         </Router>
+         <PointSender>
+            <Router>
+               <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/PickTheGame' element={<PickTheGame />} />
+                  <Route path='/RecyclingGuide' element={<RecycleGuide />} />
+                  <Route path='/Leaderboard' element={<Leaderboard />} />
+                  <Route path='/DropGame' element={<DropGame />} />
+                  <Route path='/TrashOrSmash' element={<TrashOrSmash />} />
+                  <Route path='/Recydle' element={<Recydle />} />
+               </Routes>
+            </Router>
+         </PointSender>
       </UserProvider>
    );
 }
---
+++ client/src/contexts/PointContext.tsx
@@ -0,0 +1,50 @@
+import React, { createContext, useContext, useState } from 'react';
+import { fetchData } from '../lib/utils';
+import { useUser } from './UserContext';
+
+
+type PointContextType = {
+    points: number;
+    setPoints: (value: number) => void;
+    addPoints: (amount: number) => void;
+    submit: () => Promise<void>
+};
+
+const PointContext = createContext<PointContextType | undefined>(undefined);
+
+export const PointSender: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const { username } = useUser();
+    const [points, setPoints] = useState(0);
+
+
+    const addPoints = (amount: number) => {
+        setPoints(prev => prev + amount);
+    };
+
+    const submit = async () => {
+        const baseUrl = process.env.REACT_APP_SERVER;
+        try {
+            console.log('trying')
+            await fetchData(`${baseUrl}/leaderboards`, {
+                method: 'POST',
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify({ username, points })
+            })
+            console.log('success')
+        } catch (error) {
+            console.log('failed to send points to mongo', error)
+        }
+    }
+
+    return (
+        <PointContext.Provider value={{ points, setPoints, addPoints, submit }}>
+            {children}
+        </PointContext.Provider>
+    );
+
+}
+export const usePoints = () => {
+    const context = useContext(PointContext);
+    if (!context) throw new Error('usePoints must be used inside PointProvider');
+    return context;
+};
