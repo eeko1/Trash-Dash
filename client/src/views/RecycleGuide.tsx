2138b6364bb00d93d6757dc5d45a7e8e1c9f3965
aihki <laine.nestori.julius@gmail.com>
2025-11-18 10:44:01 +0200

    fix: Making the single item styling better

--- client/src/views/RecycleGuide.tsx
+++ client/src/views/RecycleGuide.tsx
@@ -19,7 +19,7 @@ const WasteTypesList = () => {
     const { i18n } = useTranslation();
     const lang = i18n.language;
 
-     const navigate = useNavigate();
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (isSearching) return;
@@ -86,12 +86,11 @@ const WasteTypesList = () => {
                     page={page}
                 />
                 <div className='flex flex-col items-center pt-1'>
-                    <div className={`grid gap-4 w-full mx-auto overflow-auto h-80 sm:h-96
-    ${showWastes.length === 1 ? 'grid-cols-1' : 'grid-cols-2'}`}>
+                    <div className={showWastes.length === 1 ? 'flex flex-col justify-center w-full mx-auto overflow-auto h-80 sm:h-96' : 'grid grid-cols-2 gap-4 w-full mx-auto overflow-auto h-80 sm:h-96'}>
                         {showWastes.map((waste) => (
                             <button
                                 key={waste.id}
-                                className='py-6 bg-gray-800/60 text-white rounded-xl border-2 border-teal-600 shadow-lg text-xl font-bold transition hover:bg-gray-800/80 sm:py-8'
+                                className='p-6 bg-gray-800/60 text-white rounded-xl border-2 border-teal-600 shadow-lg text-xl font-bold transition hover:bg-gray-800/80 sm:py-8'
                                 onClick={() => setSelectedId(waste.id)}
                             >
                                 {waste.title}
@@ -99,19 +98,21 @@ const WasteTypesList = () => {
                         ))}
                     </div>
                 </div>
-                {selectedId !== null && (
-                    <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
-                        <div className='bg-white p-6 rounded-lg relative w-full max-h-[80vh] overflow-y-auto sm:p-8 sm:w-1/2'>
-                            <button
-                                className='absolute top-2 right-2 text-black-500 w-8 h-8 p-0 border rounded-md bg-red-600 items-center justify-center sm:w-10 sm:h-10'
-                                onClick={() => setSelectedId(null)}
-                            >
-                                &times;
-                            </button>
-                            <SpecificWaste id={selectedId} />
+                {
+                    selectedId !== null && (
+                        <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
+                            <div className='bg-white p-6 rounded-lg relative w-full max-h-[80vh] overflow-y-auto sm:p-8 sm:w-1/2'>
+                                <button
+                                    className='absolute top-2 right-2 text-black-500 w-8 h-8 p-0 border rounded-md bg-red-600 items-center justify-center sm:w-10 sm:h-10'
+                                    onClick={() => setSelectedId(null)}
+                                >
+                                    &times;
+                                </button>
+                                <SpecificWaste id={selectedId} />
+                            </div>
                         </div>
-                    </div>
-                )}
+                    )
+                }
                 <div className='flex space-x-2 mt-3'>
                     <button
                         className='px-4 py-2 bg-gray-800/60 text-white rounded hover:bg-gray-800/80 disabled:opacity-50'
@@ -129,13 +130,13 @@ const WasteTypesList = () => {
                         Next
                     </button>
                 </div>
-            </main>
+            </main >
             <footer className='bg-main_dark_turquoise text-white text-sm p-4'>
                 <div className='max-w-screen-xl mx-auto text-center'>
                     &copy; 2025 HSY. All rights reserved.
                 </div>
             </footer>
-        </div>
+        </div >
     );
 };
 
